5e3cca302497db4ca27f38c744977871ebfa59ac
asabedian <asabedia@edu.uwaterloo.ca>
2020-08-20 18:35:15 -0400

    initial commit

---
+++ src/test/kotlin/com/fieldfreshmarket/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fieldfreshmarket.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
