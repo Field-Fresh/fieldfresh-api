46db1f79f661e59d5fe01f987dd21d6217e99294
akandadai <anantkandadai@gmail.com>
2021-02-18 14:11:49 -0500

    return defaultProxy on signin

--- src/main/kotlin/com/fieldfreshmarket/api/data/auth/SignInResponseData.kt
+++ src/main/kotlin/com/fieldfreshmarket/api/data/auth/SignInResponseData.kt
@@ -2,9 +2,11 @@ package com.fieldfreshmarket.api.data.auth
 
 import com.fieldfreshmarket.api.core.cognito.CognitoJWT
 import com.fieldfreshmarket.api.model.User
+import com.fieldfreshmarket.api.model.Proxy
 
 data class SignInResponseData(
    val tokens: CognitoJWT?,
    val verificationRequired: Boolean,
-   val user: User? = null
+   val user: User? = null,
+   val defaultProxy: Proxy? = null
 )
