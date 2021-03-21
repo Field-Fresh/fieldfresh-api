abd8e76948f92fb411ca78f91b9d4bd799a35761
Ashkan Abedian <38367785+asabedia@users.noreply.github.com>
2021-03-21 12:43:51 -0400

    update start script

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 059394117896.dkr.ecr.us-east-1.amazonaws.com &&
+docker login -u AWS -p $(aws ecr get-login-password --region us-east-1) 059394117896.dkr.ecr.us-east-1.amazonaws.com &&
 docker pull 059394117896.dkr.ecr.us-east-1.amazonaws.com/fieldfresh-api && docker-compose up
